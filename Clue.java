f85b97063c5e5dc714c8912a79f34fd514266922
lkperez <112982320+lkperez@users.noreply.github.com>
2022-12-13 17:19:28 -0500

    Update Clue.java

--- Clue.java
+++ Clue.java
@@ -1,38 +1,54 @@
 public class Clue {
-    private String name; //name of the object
-    private String description; //How it looks like
-    private int amount; //How many of the object is in the room
 
+    /** String to store the clue's name */
+    private String name;
+
+    /** String to store the clue's description */
+    private String description;
+
+    /** int to store the amount of clues there are */
+    private int amount;
+
+    /** Constructor */ 
     public Clue(int amount, String name, String description) {
         this.name = name;
         this.description = description;
         this.amount = amount;
     }
-//accessors
+
+    /** Getter for name */
     public String getName() {
         return name;
     }
 
+    /** Getter for description */
     public String getDescription() {
         return description;
     }
     
+    /** Getter for amount */
     public int getamount() {
         return amount;
     }
 
+    /** Setter for name */
     public void setName(String name) {
         this.name = name;
     }
 
+    /** Setter for description */
     public void setDescription(String description) {
         this.description = description;
     }
     
+    /** Setter for amount */
     public void setAmount(int amount) {
         this.amount = amount;
     }
 
+    /** 
+     * prints a string with the attributes
+     */
     public String toString() {
         return "There is " + this.amount + " " + this.name + " and it looks like " + this.description;
     }
