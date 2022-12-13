b263e7cfeea707c8aa788405ac3bb647dc630928
lkperez <112982320+lkperez@users.noreply.github.com>
2022-12-13 17:21:49 -0500

    Update Player.java

--- Player.java
+++ Player.java
@@ -1,39 +1,59 @@
 import java.util.ArrayList;
 
 public class Player{
-    private String name; //name of the player
+
+    /** String to player's name */
+    private String name;
+
+     /** ArrayList to store card owner's name */
     private ArrayList<Clue> inventory = new ArrayList<Clue>(); //player's inventory(where they store clues)
-//accessors
+
+     /** Constructor */
     public Player(String name){
         this.name = name;
     }
 
+    /** Getter for name */
     public String getName(){
         return name;
     }
 
+    /** Getter for inventory*/
     public ArrayList<Clue> getInventory(){
         return inventory;
     }
 
+    /** Setter for name */
     public void setName(String name) {
         this.name = name;
     }
 
+    /** Setter for inventory */
     public void setInventory(ArrayList<Clue> inventory) {
         this.inventory = inventory;
     }
 
-    public void addItem(Clue Item) { // adds clue to inventory when player collects it
+    /** 
+     * adds clue to inventory when player finds one 
+     * @param Item clue player finds
+     */
+    public void addItem(Clue Item) {
         inventory.add(Item);
         System.out.println(Item.getName() + " was added to your inventory.");
     }
     
-    public void removeItem(Clue Item) { //removes clue from inventory when player puts it back
+    /** 
+     * removes clue to inventory when player finds one 
+     * @param Item clue player finds
+     */
+    public void removeItem(Clue Item) {
         inventory.remove(Item);
         System.out.println(Item.getName() + " was removed from your inventory.");
     }
 
+    /** 
+     * prints a string with the attributes
+     */
     public String toString() {
         return this.name + " has" + this.inventory;
     }
