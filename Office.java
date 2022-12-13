d42a1e453de666cf56cc88f044e46c2ff6f39197
lkperez <112982320+lkperez@users.noreply.github.com>
2022-12-13 17:20:59 -0500

    Update Office.java

--- Office.java
+++ Office.java
@@ -1,68 +1,59 @@
+         
 import java.util.Scanner;
 
 public class Office extends Rooms {
+    /** int to store amount of attempts user makes unlocking the safe */
     static int attempts;
+
+    /** boolean to store wether or not the user has the key */
     static boolean key;
 
+    /** Constructor */
     public Office(String name, String description, String objects) {
         super(name, description, objects);
         key = false;
-        attempts = 3;
 
     }
-
-    //prints out the desk statement if you choose the desk option in main  
+    /**
+     * prints out the desk statement if you choose the desk option in main  
+     */
     public static void desk(){
         System.out.println("You have looked into the drawer of the desk but there is nothing there!\n");
     }
 
-    //prints out the chair statement if you choose the chair option in main
+    /**
+     * prints out the chair statement if you choose the chair option in main 
+     */
     public static void chair(){
         System.out.println("You have looked under the chair. Here is the code: 123\n");
     }
-    //prints the safe statement and will ask you for the code and if you have the safe code, you can tell safe and it will give you the key
+
+    /**
+     * prints the safe statement and will ask you for the code and if you have the safe code, you can tell safe and it will give you the key
+     */
     public static void safe(){
         Scanner scan = new Scanner(System.in);
         System.out.println("Enter the passcode to unlock the safe and get the key!\n");
         int response = scan.nextInt();        
-        while (attempts < 4){
-        response = scan.nextInt();
-        if (response == 123){
+        while (response == 123){
+        if (response == 123 || attempts < 3){
            System.out.println("Yay! You have unlocked the safe and now have the key!\n");
            break;
-         } 
-         else {
-            System.out.println("Please try again!");
-            attempts += 1;
-            break;
-         }
-        }
-        while (attempts > 4){
-            response = scan.nextInt();
-            System.out.println("Too many attempts! Try and find the code and come back! \n");
-            break;
-         }
-     //    else{
-       //     System.out.println("Error. Answer not found.");}
-       //  else {
-         //   System.out.println("Sorry, try again!");
-           // break;}
-        //while (attempts > 3){    
-          //  System.out.println("Too many attempts! Try and find the code and come back!\n");
-            //break;
-        //}
+       }    
 
-            } 
-        
+         while (response != 123 || attempts < 3) {
+               System.out.println("Wrong code, try again!\n");
+               attempts += 1;
+          if(attempts > 3) {
+               System.out.println("Too many attempts! Try and find the code and come back!\n");
+               break;
+           } }
+       }
+    }
 
-      //   while (response != 123 || attempts < 3) {
-        //       System.out.println("Wrong code, try again!\n");
-             
-       
-    
-
-    //prints out different door statements depending on if you have the key or not
-    //problem - haven't made it so it decides if its false or not
+    /**
+     * prints out different door statements depending on if you have the key or not
+     */
     public static void door(){
        // System.out.println("You need a key to open the door. Open it using the key.");       
        if (key = true){
@@ -73,22 +64,31 @@ public class Office extends Rooms {
         }
     }
 
-    //prints inventory when you select inventory
+    /**
+     * prints inventory when you select inventory
+     */
     public static void printInventory(){
         System.out.println("Here is your inventory:\n");
     }
 
-    //prints help statement and gives you a hint
+    /**
+     * prints help statement and gives you a hint
+     */
     public static void help(){
         System.out.println("Remember, you need a code to unlock the safe. It's hidden where a person can rest! \n");
     }
 
-    //prints quit and ends the program
+    /**
+     * prints quit statement to end game if user chooses to
+     */
     public static void quit(){
         System.out.println("You have quit the game. We hope you can come back again to play!\n");
     }
 
-    //tells you to hit w so that it will move on to the menu 
+    /**
+     * tells you to hit w so that it will move on to the menu
+     * @param scnr takes in user input
+     */
     public static void wToContinue(Scanner scnr){
         String answer = "";
         while(!answer.equalsIgnoreCase("w")){
@@ -96,7 +96,10 @@ public class Office extends Rooms {
             answer = scnr.nextLine();
         }
     }
-    //our standard menu - asks you to select a number
+
+    /**
+     * Menu of the options a player can do
+     */
     public static void menu(){   
        System.out.println("What do you want to do? Select a number below:\n"
         + "1. Look in the drawer of the desk\n"
@@ -147,65 +150,3 @@ public class Office extends Rooms {
 
     }
 }
-
-/* 
-        System.out.println("Remember you need a code to unlock the safe. Its hidden where a person can rest");
-    }
-
-}
-
-     /*   Scanner scan1  = new Scanner(System.in);
-        int response = scan1.nextInt();
-        while (response == "123"){
-            System.out.println("Wrong code, try again!");
-            else if(response == "123"){
-                System.out.println("Yay! You have unlocked the safe and now have the key!");
-            }
-        }             
-               
-        scan.close();
-
-        Rooms claRooms = new Rooms("DTI", " white tile floors with white painted walls with ", " a shower, a medicine cabinet, and a toilet seat.");
-        System.out.println(claRooms);
-        System.out.println("Welcome to the Office!");
-                if  {
-
-                    System.out.print("Please put an option shown on the list\n");
-                    scan.hasNextLine();
-                } else {
-                    // Print name
-                    System.out.println("You have chosen " + name + "!");
-
-
-                }
-                        
-
-                
-Scanner scan = new Scanner(System.in);
-        menu();
-        int answer = scan.nextInt();
-
-        while(answer > 7 || answer < 1) {
-            System.out.println("pick one of the options");
-            answer = scan.nextInt();
-        }
-        switch(answer) {
-            case 1: desk();
-                    break;
-            case 2: chair();
-                    break;
-            case 3: safe();
-                    break;
-            case 4: door();
-                    break;
-            case 5: printInventory();
-                    break;
-            case 6: help();
-                    break;
-            case 7: quit();
-                    break;
-        }
-
-
-
-                */               
