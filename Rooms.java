5eb84114c29e28d89d9c4b33b3cca885f7e3829a
lkperez <112982320+lkperez@users.noreply.github.com>
2022-12-13 17:22:53 -0500

    Update Rooms.java

--- Rooms.java
+++ Rooms.java
@@ -1,37 +1,48 @@
 public class Rooms {
+    /** String to store the room's name */
     private String name;
+
+    /** String to store the rooms object's */
     private String objects;
+
+    /** String to store the room's description */
     private String description;
 
+    /** Constructor */ 
     public Rooms(String name, String description, String objects) {
         this.name = name;
         this.description = description;
         this.objects = objects;
     }
 
+    /** Getter for name */
     public String getName() {
         return name;
     }
 
+     /** Getter for description */
     public String getDescription() {
         return description;
     }
     
+     /** Getter for objects */
     public String getobjects() {
         return objects;
     }
 
+    /** Setter for name */
     public void setName(String name) {
         this.name = name;
     }
 
+    /** Setter for description */
     public void setDescription(String description) {
         this.description = description;
     }
-    
-    public void setAmount(String objects) {
-        this.objects = objects;
-    }
+
+    /** 
+     * prints a string with the attributes
+     */
     public String toString() {
         return this.name + " is a room with" + this.description + ". In this room there is a " + this.objects + ".";
     }
